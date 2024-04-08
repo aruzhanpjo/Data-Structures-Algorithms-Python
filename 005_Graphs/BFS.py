a24b80d36da53c2490a7bca504b21c6da15c838e
aruzhanpjo <aruzhan.betigenova@mnsu.edu>
2024-04-08 12:50:01 -0500

    added a graph example

--- 005_Graphs/BFS.py
+++ 005_Graphs/BFS.py
@@ -1,4 +1,4 @@
-def BFS(startV):
+def BFS(graph,startV):
     queue = []
     discovered = set()
     queue.append(startV)
@@ -6,8 +6,20 @@ def BFS(startV):
     while queue != []:
         cur = queue.pop(0)
         print(cur)
-        for neighbor in queue[cur]:
+        
+        for neighbor in graph[cur]:
             if neighbor not in discovered:
                 queue.append(neighbor)
                 discovered.add(neighbor)
                 
+Graph = { 1: [2,3,4,5],
+         2: [1, 6],
+         3: [1,6],
+         4: [1, 7],
+         5: [1, 7],
+         6: [2,3, 8],
+         7: [4, 5, 8]}
+
+BFS(Graph, 1)
+
+
